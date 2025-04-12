d8425aa69c040a37b301d7bd90401319a2f0e64c
LukasCadman <lukas.cadman@dcmail.ca>
2025-04-11 22:51:18 -0400

    Navigation and clean up
    wrapped the navigation bar with the nav tag. aswell as cleaning up br tags, changing the td table tags to th tags.

--- Assignment5/main.js
+++ Assignment5/main.js
@@ -1,6 +1,7 @@
 /*
 Name: Lukas Cadman
 Student Number: 100880949
+File: main.js
 Date: April 10th, 2025
 Description: This is the Java Script file for Assignment 5, where I will be correcting mistakes and added accessibility options to the web page.
 */
@@ -12,16 +13,30 @@ const commentWrapper = document.querySelector('.comment-wrapper');
 
 commentWrapper.style.display = 'none';
 
-showHideBtn.onclick = function() {
-  let showHideText = showHideBtn.textContent;
-  if(showHideText === 'Show comments') {
-    showHideBtn.textContent = 'Hide comments';
-    commentWrapper.style.display = 'block';
+// initializes the ARIA state
+showHideBtn.setAttribute('aria-expanded', 'false');
+
+// Handles clicking and keyboard events
+showHideBtn.addEventListener('click', toggleComments);
+showHideBtn.addEventListener('keydown', (event) => { 
+  if (event.key === 'Enter', event.key === ' ') {
+    event.preventDefault();
+    toggleComments();
+  }
+});
+
+function toggleComments() {
+  const isExpanded = showHideBtn.getAttribute('aria-expanded') === 'true';
+  if (!isExpanded) {
+    showHideBtn.textContent = 'Hide comments'
+    commentWrapper.style.display = 'block'
+    showHideBtn.setAttribute('aria-expanded', 'true');
   } else {
     showHideBtn.textContent = 'Show comments';
     commentWrapper.style.display = 'none';
+    showHideBtn.setAttribute('aria-expanded', 'false');
   }
-};
+}
 
 // functionality for adding a new comment via the comments form
 
@@ -33,7 +48,7 @@ const list = document.querySelector('.comment-container');
 form.onsubmit = function(e) {
   e.preventDefault();
   submitComment();
-};
+};  
 
 function submitComment() {
   const listItem = document.createElement('li');
